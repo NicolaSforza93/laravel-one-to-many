7b0b1f5de4b75cb78757995844a0d8c8f64a6d95
NicolaSforza93 <nicola.sforza93@gmail.com>
2024-01-09 16:51:22 +0100

    ADD - defined one to many relationship

--- database/migrations/2024_01_09_152032_add_type_id_foreing_key_to_projects_table.php
+++ database/migrations/2024_01_09_152032_add_type_id_foreing_key_to_projects_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('set null')->onUpdate('cascade');
         });
     }
 
@@ -22,7 +24,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropForeign(['type_id']);
+
+            $table->dropColumn('type_id');
         });
     }
 };
