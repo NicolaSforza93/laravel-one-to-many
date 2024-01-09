91830e8a5936cba7cdc4c3679ef706d5d7f0f0a1
NicolaSforza93 <nicola.sforza93@gmail.com>
2024-01-09 16:22:11 +0100

    ADD - migration for table projects

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,14 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = ['FrontEnd', 'BackEnd', 'FullStack', 'Design'];
+
+        foreach ($types as $type_name) {
+            $new_type = new Type();
+
+            $new_type->name = $type_name;
+
+            $new_type->save();
+        }
     }
 }
